8e955dd7406426bb3fc013d14d48edf5837abfcd
miggules <miggules@gmail.com>
2020-03-12 11:00:54 +0100

    DRINK CATALOG (unfinished)

--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <?php
 
-$nav = [
+$products = [
     [
-        'url' => '/index.php',
-        'label' => 'Home',
-        'links' => [
-            'url' => '/',
-            'label' => 'help'
-        ],
-        [
-            'url' => '/',
-            'label' => 'me'
-        ],
-        [
-            'url' => '/',
-            'label' => 'please'
-        ],
-        [
-            'url' => '/',
-            'label' => 'god'
-        ]
+        'name' => 'Stumbro Degtinela',
+        'price' => 6.49,
+        'image' => '/assets/stumbras.jpg',
     ],
     [
-        'url' => '/',
-        'label' => 'About'
+        'name' => 'Balzamas',
+        'price' => 9.89,
+        'image' => '/assets/balzamas.jpg',
     ],
     [
-        'url' => '/',
-        'label' => 'Contact'
+        'name' => 'Absolut',
+        'price' => 14.99,
+        'image' => '/assets/absolut.png',
     ],
     [
-        'url' => '/',
-        'label' => 'Portfolio'
+        'name' => 'Baileys',
+        'price' => 8.69,
+        'image' => '/assets/baileys.jpg',
     ]
 ];
 
-// var_dump($_SERVER);
+$h1 = 'Drink catalog';
+
+$product = [];
+
+foreach ($products as $product_key => $product) {
+
+    $name_key = $products[$product_key]['name'];
+    $price_key = $products[$product_key]['price'];
+    $image_key = $products[$product_key]['image'];
+
+    $product = [
+        'name' => $name_key,
+        'price' => $price_key,
+        'image' => $image_key
+    ];
+}
 
 ?>
 <html lang="en">
@@ -43,10 +46,29 @@ $nav = [
     <title>
         php
     </title>
-    <link href="/assets/css/nav.css" type="text/css" rel="stylesheet">
+    <link href="/assets/css/style.css" type="text/css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 </head>
-    <body>
-        <?php include 'templates/nav.php';?>
-    </body>
+<body>
+<main>
+    <h1><?php print $h1; ?></h1>
+    <div class="wrapper">
+        <div class="products">
+            <?php foreach ($products as $product): ?>
+            <div class="product">
+                <img src="<?php print $product['image']; ?>">
+                <div class="info">
+                    <h2>
+                        <?php print $product['name']; ?>
+                    </h2>
+                    <h3>
+                        <?php print $product['price']; ?>
+                    </h3>
+                </div>
+            </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+</main>
+</body>
 </html>
