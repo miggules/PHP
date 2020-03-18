3446094f76eb7b6ec6a1d91e4e2e4aa3825ed9d1
miggules <miggules@gmail.com>
2020-03-18 09:31:24 +0100

    functions

--- index.php
+++ index.php
@@ -1,27 +1,53 @@
 <?php
 
-$a = rand(10, 50);
-$b = rand(20, 60);
-$c = rand(1, 10);
-
-function sum($x, $y)
-{
-    $sum = $x + $y;
-    return "$x and $y equals to $sum";
-}
+$a = rand(1000, 10000);
+$b = 750.49;
 
 function prime($x)
 {
     for ($i = 2; $i <= ($x/2); $i++) {
         if ($x % $i == 0) {
-            return "$x is not a prime";
+            return false;
         }
     }
-    return "$x is a prime";
+    return true;
 }
 
-$h1 = sum($a, $b);
-$h2 = prime($c);
+function sum($x, $y) {
+    return $sum = $x - $y;
+}
+
+function show_name($full_name) {
+    return 'Hello, this is ' . $full_name;
+}
+
+function srtotime($now) {
+    return date("F j, Y, g:i a") . "\n";
+}
+
+function truth($item) {
+    return "choke on $item";
+}
+
+function n($x) {
+    return $x + 1;
+}
+
+
+function x($z) {
+    return n($z) + 1;
+}
+
+$x = 1;
+
+print x($x);
+
+$hello = show_name('David Rogers') . ' and the date is ' . srtotime("now");
+$money = "So I got paid $a today, but my sugar baby wants $b for her new set of shoes";
+$lemon_stealing_whore = "If I don't pay her, I'll have no one who loves me - so now I have " . sum($a, $b);
+$sad_life = 'Now, I know money cannot buy happiness, but.. it can buy a damn good actress. Even though, deep down, I know she probably wants me to go ' . truth('tidepods') . ' like she often says I make her ' . truth('dick');
+
+
 
 ?>
 <html lang="en">
@@ -35,8 +61,10 @@ $h2 = prime($c);
 <body>
 <main>
     <div class="wrapper">
-        <h1><?php print $h1; ?></h1>
-        <h2><?php print $h2; ?></h2>
+        <p><?php print $hello; ?></p>
+        <p><?php print $money; ?></p>
+        <p><?php print $lemon_stealing_whore; ?></p>
+        <p><?php print $sad_life; ?></p>
     </div>
 </main>
 </body>
